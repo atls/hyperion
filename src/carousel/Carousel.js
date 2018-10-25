5b86149bc674238cb749bee1993dccaa886162c7
Andrew Ghostuhin <me@torinasakura.name>
2018-10-25 14:59:38 +0300

    feature(ui/carousel): creating dots navigation component feature(ui/carousel): creating content markup with contentSizer utility
    Signed-off-by: Andrew Ghostuhin <me@torinasakura.name>

--- src/carousel/Carousel.js
+++ src/carousel/Carousel.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react'
 import { Column, Layout } from 'flex-layouts'
+import Content from './content/Content'
+import Dots from './dots/Dots'
 
 class Carousel extends Component {
+  state = {
+    active: 0,
+  }
 
-  render() {
+  onChange = (active) => {
+    this.setState({ active })
+  }
+
+
+  render () {
     const { children } = this.props
+    const { active } = this.state
 
     return (
       <Column>
         <Layout grow={1}>
-          {children}
+          <Content
+            active={active}
+            onChange={this.onChange}
+          >
+            {children}
+          </Content>
         </Layout>
         <Layout>
-          {children}
+          <Dots
+            active={active}
+            onChange={this.onChange}
+          >
+            {children}
+          </Dots>
         </Layout>
       </Column>
     )
