13dc1c9488e2cb2845295dac81e3569f7afa0d4a
ilyassab98 <ilyassab98@gmail.com>
2020-06-02 16:05:12 +0300

    refactor(packages): add mobile swipe to carousel and slideshow

--- packages/slideshow/src/Slideshow.tsx
+++ packages/slideshow/src/Slideshow.tsx
@@ -1,8 +1,10 @@
 import styled                                 from '@emotion/styled'
 import React, { useEffect, useRef, useState } from 'react'
+import { Swipeable }                          from 'react-swipeable'
 
 import { contentWidth }                       from '@atlantis-lab/utils'
 
+import { Circles }                            from './Circles'
 import { Slices }                             from './Slices'
 
 const Container = styled.div({
@@ -11,10 +13,24 @@ const Container = styled.div({
   position: 'relative',
 })
 
-export const Slideshow = ({ children, time }) => {
+export const Slideshow = ({ children, time = 10000, controlsType = 'slices' }) => {
   const [slide, setSlide] = useState(0)
   const [containerWidth, setContainerWidth] = useState(null)
   const containerNode = useRef(null)
+
+  const swiped = data => {
+    if (data.dir === 'Left') {
+      if (slide < children.length - 1) {
+        setSlide(slide + 1)
+      } else {
+        setSlide(0)
+      }
+    } else if (slide > 0) {
+      setSlide(slide - 1)
+    } else {
+      setSlide(children.length - 1)
+    }
+  }
 
   useEffect(() => {
     setContainerWidth(contentWidth(containerNode.current))
@@ -33,13 +49,31 @@ export const Slideshow = ({ children, time }) => {
 
   return (
     <Container ref={containerNode}>
-      {children[slide]}
-      <Slices
-        slides={children}
-        currentSlide={slide}
-        containerWidth={containerWidth}
-        lastSlide={children.length - 1}
-      />
+      <Swipeable
+        onSwiped={data => swiped(data)}
+        preventDefaultTouchmoveEvent
+        trackMouse
+        trackTouch
+        delta={15}
+      >
+        {children[slide]}
+      </Swipeable>
+      {controlsType === 'circles' && (
+        <Circles
+          slides={children}
+          currentSlide={slide}
+          containerWidth={containerWidth}
+          lastSlide={children.length - 1}
+        />
+      )}
+      {controlsType === 'slices' && (
+        <Slices
+          slides={children}
+          currentSlide={slide}
+          containerWidth={containerWidth}
+          lastSlide={children.length - 1}
+        />
+      )}
     </Container>
   )
 }
