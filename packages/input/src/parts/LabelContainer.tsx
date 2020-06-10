745efcfcda68a54807001064bddeb122785d9bbf
Andrew Ghostuhin <me@torinasakura.name>
2020-06-10 14:17:40 +0300

    refactor(packages): add flexbox in packages/modal, fix Container render in input-label-addon
    affects: @atlantis-lab/input, @atlantis-lab/modal

--- packages/input/src/parts/LabelContainer.tsx
+++ packages/input/src/parts/LabelContainer.tsx
@@ -19,21 +19,19 @@ export const LabelContainer = ({
   labelLeft,
   children,
 }: any) => {
-  if (!labelText) {
-    return children
-  }
-
   return (
     <Container>
-      <Label
-        color={labelColor}
-        fontSize={labelFontSize}
-        fontWeight={labelFontWeight}
-        top={labelTop}
-        left={labelLeft}
-      >
-        {labelText}
-      </Label>
+      {labelText && (
+        <Label
+          color={labelColor}
+          fontSize={labelFontSize}
+          fontWeight={labelFontWeight}
+          top={labelTop}
+          left={labelLeft}
+        >
+          {labelText}
+        </Label>
+      )}
       {children}
     </Container>
   )
