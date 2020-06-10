864862e634274eeef484abdc64b41a27eeebf3ac
ilyassab98 <ilyassab98@gmail.com>
2020-06-10 13:14:58 +0300

    feat(input): add input-label-addon

---
+++ packages/input/src/parts/Label.tsx
@@ -0,0 +1,15 @@
+import React                           from 'react'
+import { color, position, typography } from 'styled-system'
+
+import styled                          from '@emotion/styled'
+
+export const Label = styled.label(
+  {
+    position: 'absolute',
+    backgroundColor: 'white',
+    padding: '0 10px',
+  },
+  color,
+  position,
+  typography
+)
