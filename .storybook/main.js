92bd7b340862014b0e5fd388decb237347842ae8
TFK70 <buldozik.nikitosik@mail.ru>
2021-01-23 14:34:33 +0500

    refactor(carousel): refactor stories
    affects: @atlantis-lab/carousel
    
    - Merged two stories into a single one

--- .storybook/main.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  stories: [
-    '../packages/**/src/stories/*.story.mdx',
-    '../packages/**/src/stories/*.story.@(js|jsx|ts|tsx)',
-  ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-}
