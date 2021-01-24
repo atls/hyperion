d09ff0b31f6beee1c6bdd228732e65af8c8cf250
TFK70 <buldozik.nikitosik@mail.ru>
2021-01-24 16:48:04 +0500

    feat(carousel & slideshow): add stories
    affects: @atlantis-lab/carousel, @atlantis-lab/slideshow
    
    - Refactored carousel stories
    - Added slideshow stories
    
    ISSUES CLOSED: #141

--- packages/slideshow/src/Dots/index.tsx
+++ packages/slideshow/src/Dots/index.tsx
@@ -1,4 +1,5 @@
 import React, { FC, Fragment } from 'react'
+import { useTheme }            from '@emotion/react'
 
 import { unique }              from '@atlantis-lab/utils'
 
@@ -6,12 +7,16 @@ import { ControlsProps }       from '../types'
 import { Container }           from './Container'
 import { Dot }                 from './Dot'
 
-export const Dots: FC<ControlsProps> = ({ slides, currentSlide, lastSlide, containerWidth }) => (
-  <Container containerWidth={containerWidth}>
-    {slides.map((item, index) => (
-      <Fragment key={`${unique()}`}>
-        <Dot active={index === currentSlide} lastSlide={index === lastSlide} />
-      </Fragment>
-    ))}
-  </Container>
-)
+export const Dots: FC<ControlsProps> = ({ slides, currentSlide, lastSlide, containerWidth }) => {
+  const theme = useTheme()
+
+  return (
+    <Container containerWidth={containerWidth}>
+      {slides.map((item, index) => (
+        <Fragment key={`${unique()}`}>
+          <Dot active={index === currentSlide} lastSlide={index === lastSlide} theme={theme} />
+        </Fragment>
+      ))}
+    </Container>
+  )
+}
