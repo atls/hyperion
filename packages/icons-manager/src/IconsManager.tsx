21ad8e96f0c943e9574f1af8c483efe09a798549
TFK70 <buldozik.nikitosik@mail.ru>
2021-01-26 15:41:29 +0500

    feat(icons-manager): add story
    affects: @atlantis-lab/icons-manager
    
    ISSUES CLOSED: #156

--- packages/icons-manager/src/IconsManager.tsx
+++ packages/icons-manager/src/IconsManager.tsx
@@ -1,6 +1,6 @@
 import styled                             from '@emotion/styled'
 import React, { FC, useEffect, useState } from 'react'
-import { useTheme }                       from 'emotion-theming'
+import { useTheme }                       from '@emotion/react'
 
 interface Props {
   color?: string
@@ -15,9 +15,12 @@ const IconUI = styled.div({
 })
 
 export const IconsManager: FC<Props> = ({ children, hoverColor, clickedColor, color }) => {
-  const theme = useTheme<any>()
+  const theme = useTheme()
+  // @ts-ignore
   const themeColor = theme.colors[color] || color
+  // @ts-ignore
   const themeHoverColor = theme.colors[hoverColor] || hoverColor
+  // @ts-ignore
   const themeClickedColor = theme.colors[clickedColor] || clickedColor
 
   const [currentColor, setCurrentColor] = useState(themeColor)
