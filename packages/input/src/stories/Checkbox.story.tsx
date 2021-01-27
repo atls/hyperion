482c21fe7af2391db975dd7026f67288e71e5d21
TFK70 <buldozik.nikitosik@mail.ru>
2021-01-27 15:43:01 +0500

    feat(input): add story
    affects: @atlantis-lab/grid, @atlantis-lab/input, @atlantis-lab/storybook
    
    ISSUES CLOSED: #158

---
+++ packages/input/src/stories/Checkbox.story.tsx
@@ -0,0 +1,15 @@
+import React        from 'react'
+import { useTheme } from '@emotion/react'
+
+import { Checkbox } from '../Checkbox'
+
+export const ExampleCheckbox = () => {
+  const theme = useTheme()
+
+  return <Checkbox theme={theme}>Check this</Checkbox>
+}
+
+export default {
+  title: 'Input',
+  component: ExampleCheckbox,
+}
