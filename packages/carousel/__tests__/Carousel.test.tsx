fce8bfbc7953c74b97cf0ad8903f3e46ac27508f
TFK70 <buldozik.nikitosik@mail.ru>
2021-01-30 15:14:53 +0500

    feat(carousel): add test
    affects: @atlantis-lab/carousel
    
    - Deleted "assets" folder from src
    - Polished Carousel.tsx
    - Polished Carousel.story.tsx
    
    ISSUES CLOSED: #171

---
+++ packages/carousel/__tests__/Carousel.test.tsx
@@ -0,0 +1,42 @@
+import React        from 'react'
+import renderer     from 'react-test-renderer'
+
+import { Carousel } from '../src/index'
+
+it('Carousel should render correctly', () => {
+  const defaultTheme = {
+    colors: {
+      green: '#0f0',
+      black: '#000',
+      white: '#fff',
+      blue: '#00f',
+      red: '#f00',
+      lightGray: 'gray',
+    },
+    shadows: {
+      controls: '0 0 10px rgba(0,0,0,0.5)',
+    },
+    borders: {
+      controls: '2px solid black',
+    },
+  }
+
+  const tree = renderer
+    .create(
+      <Carousel
+        controlWidth={50}
+        controlHeight={50}
+        controlRight={0}
+        controlLeft={0}
+        controlTop={100}
+        isOverflowHidden={false}
+        isSquareControls={false}
+        theme={defaultTheme}
+      >
+        <h1>{'bigtext'.repeat(100)}</h1>
+      </Carousel>
+    )
+    .toJSON()
+
+  expect(tree).toMatchSnapshot()
+})
