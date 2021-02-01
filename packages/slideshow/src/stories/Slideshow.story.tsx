d3758314986451dfaea2372547567f72e02b3305
TFK70 <buldozik.nikitosik@mail.ru>
2021-02-01 19:56:22 +0500

    fix(slideshow): fix margins
    affects: @atlantis-lab/slideshow
    
    - Also updated snapshot
    
    ISSUES CLOSED: #167

--- packages/slideshow/src/stories/Slideshow.story.tsx
+++ packages/slideshow/src/stories/Slideshow.story.tsx
@@ -1,13 +1,22 @@
-import React         from 'react'
+import React           from 'react'
 
-import { Slideshow } from '../index'
+import { Column, Row } from '@atlantis-lab/layout'
+
+import { Slideshow }   from '../index'
 
 export const ExampleSlideshow = ({ time, transition = '.5s', controlsType }) => (
-  <Slideshow time={time} transition={transition} controlsType={controlsType}>
-    <h1>Child 1</h1>
-    <h1>Child 2</h1>
-    <h1>Child 3</h1>
-  </Slideshow>
+  <Column>
+    <Row>
+      <Slideshow time={time} transition={transition} controlsType={controlsType} width='10%'>
+        <h1>Child 1 Child 1 Child 1 Child 1 Child 1 </h1>
+        <h1>Child 2 Child 2 Child 2 Child 2 Child 2 Child 2 </h1>
+        <h1>Child 3</h1>
+      </Slideshow>
+    </Row>
+    <Row>
+      <h1>An element</h1>
+    </Row>
+  </Column>
 )
 
 export default {
