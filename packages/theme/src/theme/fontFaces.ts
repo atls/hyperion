840db41fd4d3100f15592fef7363091511b4ce4d
TFK70 <buldozik.nikitosik@mail.ru>
2021-02-15 21:36:54 +0500

    fix(theme): clear fontFaces
    affects: @atlantis-lab/theme

--- packages/theme/src/theme/fontFaces.ts
+++ packages/theme/src/theme/fontFaces.ts
@@ -1,22 +1,3 @@
 export const fontFaces: { family: string, weight: number, type: string}[]  = [
-  {
-    family: 'Inter',
-    weight: 600,
-    type: 'SemiBold',
-  },
-  {
-    family: 'Inter',
-    weight: 700,
-    type: 'Bold',
-  },
-  {
-    family: 'Inter',
-    weight: 500,
-    type: 'Medium',
-  },
-  {
-    family: 'Inter',
-    weight: 400,
-    type: 'Regular',
-  },
+
 ]
