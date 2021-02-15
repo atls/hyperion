151c6a98c6e19a6f85646c9e46d293b01fcf07f2
TFK70 <buldozik.nikitosik@mail.ru>
2021-02-15 22:37:21 +0500

    chore(theme): clear all theme files
    affects: @atlantis-lab/theme
    
    - Also move to atlantis-lab config

--- packages/theme/src/theme/borders.ts
+++ packages/theme/src/theme/borders.ts
@@ -1,8 +1,3 @@
 export const borders: {[key: string]: string}  = {
-  black: '1px solid rgba(0, 0, 0, 0.08)',
-  codgray: '1px solid rgba(0, 0, 0, 0.06)',
-  white: '1px solid rgba(255, 255, 255, 0.32)',
-  woodsmoke: '1px solid rgba(0, 0, 0, 0.20)',
-  mercury: '1px solid rgb(229, 229, 229)',
-  gallery: '1px solid rgb(235, 235, 235)',
+
 }
