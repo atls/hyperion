47919f9039f0b45dd74838b77a52e00e98c13fd4
TFK70 <buldozik.nikitosik@mail.ru>
2021-03-08 18:46:44 +0500

    fix(theme): lint
    affects: @atlantis-lab/theme

--- packages/theme/src/globals.ts
+++ packages/theme/src/globals.ts
@@ -4,22 +4,22 @@ import { FontFaces }       from './theme'
 import { injectFontFaces } from './utils'
 
 export const injectGlobalStyles = (
-    fontsPath: string,
-    fontFaces: FontFaces,
-    html: {} = {},
-    body: {} = {
-        WebkitFontSmoothing: 'antialiased',
-        WebkitOverflowScrolling: 'touch',
-        height: '100%',
-        margin: 0,
-    },
-    ...rest
+  fontsPath: string,
+  fontFaces: FontFaces,
+  html: {} = {},
+  body: {} = {
+    WebkitFontSmoothing: 'antialiased',
+    WebkitOverflowScrolling: 'touch',
+    height: '100%',
+    margin: 0,
+  },
+  ...rest
 ) => {
-    injectFontFaces(fontFaces, fontsPath)
+  injectFontFaces(fontFaces, fontsPath)
 
-    injectGlobal({
-        html,
-        body,
-        ...rest,
-    })
+  injectGlobal({
+    html,
+    body,
+    ...rest,
+  })
 }
