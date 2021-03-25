d2eba87f3e3c5fc1f485dcfebefe91bad4babd84
Nikita Elfimov <buldozik.nikitosik@mail.ru>
2021-03-26 00:36:17 +0500

    feat(select): add configurable styles
    affects: @atlantis-lab/select
    
    ISSUES CLOSED: #252

--- packages/select/src/stories/Select.story.tsx
+++ packages/select/src/stories/Select.story.tsx
@@ -1,13 +1,32 @@
-import React                        from 'react'
-import { Option }                   from 'rc-select'
+import React      from 'react'
+import { Option } from 'rc-select'
 
-import { Select as TemplateSelect } from '..'
+import { Select } from '..'
 
-const Select = (props) => <TemplateSelect {...props} />
-
-export const ExampleSelect = () => {
+export const ExampleSelect = ({
+  multiple,
+  showSearch,
+  invalid,
+}: {
+  multiple: boolean
+  showSearch: boolean
+  invalid: boolean
+}) => {
   return (
-    <Select>
+    <Select
+      multiple={multiple}
+      showSearch={showSearch}
+      invalid={invalid}
+      fontFamily='Inter'
+      fontWeight={600}
+      fontSize='12px'
+      minHeight='50px'
+      width='100%'
+      border='1px solid rgba(0, 0, 0, 0.08)'
+      borderRadius='4px'
+      backgroundColor='#ffffff'
+      placeholder='Choose your option'
+    >
       <Option value='One'>One</Option>
       <Option value='Two'>Two</Option>
     </Select>
---
+++ packages/select/src/styles.ts
@@ -0,0 +1,61 @@
+import { css } from '@emotion/css'
+
+export const dropdownStyles = ({
+  backgroundColor,
+  borderRadius,
+  maxHeight,
+  color,
+  activeColor,
+  fontFamily,
+  fontWeight,
+  fontSize,
+}) => css`
+  background-color: ${backgroundColor || '#fff'};
+  border-radius: ${borderRadius || '4px'};
+  box-shadow: 3px 24px 40px 0 rgba(0, 0, 0, 0.08);
+  box-sizing: border-box;
+  z-index: 100;
+  position: absolute;
+  outline: none;
+  max-height: ${maxHeight || '150px'};
+  overflow: scroll;
+  cursor: pointer;
+  &.rc-select-dropdown-hidden,
+  &.rc-select-dropdown-hidden:empty {
+    display: none;
+  }
+  .rc-select-dropdown-menu {
+    outline: none;
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    z-index: 9999;
+    box-sizing: border-box;
+    .rc-select-dropdown-menu-item {
+      font-family: ${fontFamily || 'Inter'};
+      font-weight: ${fontWeight || 600};
+      font-size: ${fontSize || '12px'};
+      padding: 9px 20px;
+      color: ${color || '#99999999'};
+      outline: none;
+    }
+    .rc-select-dropdown-menu-item.hidden {
+      display: none;
+    }
+    .rc-select-dropdown-menu-item:first-of-type.hidden + .rc-select-dropdown-menu-item {
+      padding-top: 19px;
+    }
+    .rc-select-dropdown-menu-item:last-of-type.hidden + .rc-select-dropdown-menu-item {
+      padding-bottom: 19px;
+    }
+    .rc-select-dropdown-menu-item:first-of-type {
+      padding-top: 19px;
+    }
+    .rc-select-dropdown-menu-item:last-of-type {
+      padding-bottom: 19px;
+    }
+    .rc-select-dropdown-menu-item-active {
+      color: ${activeColor || '#bebebe'};
+    }
+  }
+`
