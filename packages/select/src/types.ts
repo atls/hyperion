0d1952d2565c6079074136187c3af866cf25bd66
Nikita Elfimov <buldozik.nikitosik@mail.ru>
2021-03-26 01:09:45 +0500

    feat(select): add SelectProps type from rc-select
    affects: @atlantis-lab/select

--- packages/select/src/types.ts
+++ packages/select/src/types.ts
@@ -1,6 +1,12 @@
+import { ISelectProps }                                          from 'rc-select/lib/PropTypes'
 import { BorderProps, ColorProps, LayoutProps, TypographyProps } from 'styled-system'
 
-export interface SelectProps extends BorderProps, ColorProps, TypographyProps, LayoutProps {
+export interface SelectProps
+  extends BorderProps,
+    ColorProps,
+    TypographyProps,
+    LayoutProps,
+    ISelectProps {
   children: any
   showSearch: boolean
   multiple: boolean
