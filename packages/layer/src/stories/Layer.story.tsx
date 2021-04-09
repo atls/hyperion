92531c85fb9fb5c9734d0c973df996bab0d1d780
Nikita Elfimov <ne@atls.team>
2021-04-09 15:25:27 +0500

    chore(layer): remove dependency
    affects: @atlantis-lab/layer
    
    - Also modifed story a bit
    
    ISSUES CLOSED: #272

--- packages/layer/src/stories/Layer.story.tsx
+++ packages/layer/src/stories/Layer.story.tsx
@@ -2,14 +2,14 @@ import React, { useState } from 'react'
 
 import { Layer }           from '..'
 
-export const ExampleLayer = () => {
+export const ExampleLayer = ({ onClickOutside = true }: { onClickOutside: boolean }) => {
   const [active, setActive] = useState<boolean>(true)
 
   return (
     <>
       <button type='button'>Fake button</button>
-      <Layer active={active} setActive={setActive} border='1px solid red' borderRadius='50'>
-        Check this
+      <Layer active={active} setActive={setActive} border='1px solid red' borderRadius='50' padding={100}>
+        It wont disappear onClickInside
       </Layer>
       <button type='button' onClick={() => setActive(!active)}>
         Toggle layer
