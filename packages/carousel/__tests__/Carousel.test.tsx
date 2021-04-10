67b30d2bad537f00c085e0fb10d34d9bf92e01a9
Nikita Elfimov <ne@atls.team>
2021-04-10 13:08:42 +0500

    chore: remove unused vars
    affects: @atlantis-lab/carousel, @atlantis-lab/icons-manager

--- packages/carousel/__tests__/Carousel.test.tsx
+++ packages/carousel/__tests__/Carousel.test.tsx
@@ -4,23 +4,6 @@ import renderer     from 'react-test-renderer'
 import { Carousel } from '../src/index'
 
 it('Carousel should render correctly', () => {
-  const defaultTheme = {
-    colors: {
-      green: '#0f0',
-      black: '#000',
-      white: '#fff',
-      blue: '#00f',
-      red: '#f00',
-      lightGray: 'gray',
-    },
-    shadows: {
-      controls: '0 0 10px rgba(0,0,0,0.5)',
-    },
-    borders: {
-      controls: '2px solid black',
-    },
-  }
-
   const tree = renderer
     .create(
       <Carousel
