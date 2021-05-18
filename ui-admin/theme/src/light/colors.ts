380faed6a5939daca738d12f56ab2d7591a25c51
Andrew Ghostuhin <me@torinasakura.name>
2021-05-19 00:28:33 +0300

    feature(au-admin): add theme with switch mode

---
+++ ui-admin/theme/src/light/colors.ts
@@ -0,0 +1,7 @@
+export const colors = {
+  button: {
+    primary: {
+      fill: '#8676FF',
+    },
+  },
+}
---
+++ ui-admin/theme/src/mode/index.ts
@@ -0,0 +1,4 @@
+export * from './theme-mode.interfaces'
+export * from './theme-mode.context'
+export * from './theme-mode.store'
+export * from './use-theme-mode.hook'
