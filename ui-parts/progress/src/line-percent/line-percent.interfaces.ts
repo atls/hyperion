d9b01343993851969f1575c139a72222e2c9f65c
Andrew Ghostuhin <me@torinasakura.name>
2021-06-10 16:28:30 +0300

    chore: init

---
+++ ui-parts/progress/src/line-percent/line-percent.interfaces.ts
@@ -0,0 +1,7 @@
+import { ProgressLinecap } from '../progress.interfaces'
+
+export interface LinePercentElementProps {
+  backgroundColor?: string
+  backgroundImage?: string
+  strokeLinecap?: ProgressLinecap
+}
