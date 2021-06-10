dd3e6093e560b5a54e26912a1ffa95ab238ebeea
Nikita Elfimov <ne@atls.team>
2021-06-10 14:23:08 +0500

    chore(text): add format prop
    ISSUES CLOSED: #297

--- ui-parts/text/src/currency.component.tsx
+++ ui-parts/text/src/currency.component.tsx
@@ -3,9 +3,9 @@ import { FC }            from 'react'
 import { Text }          from './text.component'
 import { CurrencyProps } from './currency.interface'
 
-const Currency: FC<CurrencyProps> = ({ amount, currency, ...props }) => (
+const Currency: FC<CurrencyProps> = ({ amount, currency, format, ...props }) => (
   <Text {...props}>
-    {new Intl.NumberFormat('ru-RU', { style: 'currency', currency })
+    {new Intl.NumberFormat(format, { style: 'currency', currency })
       .format(amount)
       .replace(',00', '')}
   </Text>
@@ -14,6 +14,7 @@ const Currency: FC<CurrencyProps> = ({ amount, currency, ...props }) => (
 Currency.defaultProps = {
   amount: 0,
   currency: 'USD',
+  format: 'ru-RU',
 }
 
 export { Currency }
