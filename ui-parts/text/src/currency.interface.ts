d52ffbd50ce12322575e8bf11af867ee4c546184
Nikita Elfimov <ne@atls.team>
2021-06-10 14:20:33 +0500

    refactor(text): use standards
    ISSUES CLOSED: #297

--- ui-parts/text/src/currency.interface.ts
+++ ui-parts/text/src/currency.interface.ts
@@ -5,6 +5,5 @@ import { TextProps }       from './text.interfaces'
 export interface CurrencyProps
   extends StyledComponent<TextProps, any, HTMLAttributes<HTMLSpanElement>> {
   amount: number
-  currency: 'rub' | 'usd' | 'eur' | string
-  currencySignatures?: { [key: string]: string }
+  currency: string
 }
