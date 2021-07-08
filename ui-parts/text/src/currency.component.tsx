2c3d09b1c52dfe5e65b5ccad41a2d59764e65850
Nikita Elfimov <ne@atls.team>
2021-07-08 19:15:44 +0500

    fix: currency types
    - Added stories for currency
    
    ISSUES CLOSED: #322

--- ui-parts/text/src/currency.component.tsx
+++ ui-parts/text/src/currency.component.tsx
@@ -1,20 +1,25 @@
 import React             from 'react'
 import { FC }            from 'react'
+import styled            from '@emotion/styled'
+
 import { Text }          from './text.component'
 import { CurrencyProps } from './currency.interface'
 
-const Currency: FC<CurrencyProps> = ({ amount, currency, locale, ...props }) => (
-  <Text {...props}>
+const CurrencyChildren: FC<CurrencyProps> = ({ amount, currency, locale, ...props }) => (
+  <span {...props}>
     {new Intl.NumberFormat(locale, { style: 'currency', currency })
       .format(amount)
       .replace(',00', '')}
-  </Text>
+  </span>
 )
+
+const Currency = styled(Text.withComponent(CurrencyChildren))()
 
 Currency.defaultProps = {
   amount: 0,
   currency: 'USD',
   locale: 'ru-RU',
+  ...Text.defaultProps,
 }
 
 export { Currency }
