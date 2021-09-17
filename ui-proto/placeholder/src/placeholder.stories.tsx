74d177bed1d2486ae3f875584460a4897ccfe2e3
Andrew Ghostuhin <me@torinasakura.name>
2021-09-18 00:10:41 +0300

    feat(placeholder): init

---
+++ ui-proto/placeholder/src/placeholder.stories.tsx
@@ -0,0 +1,58 @@
+import React                          from 'react'
+
+import { Box }                        from '@atls-ui-proto/layout'
+
+import { ImagePlaceholder }           from './image'
+import { ImageBoxPlaceholder }        from './image-box'
+import { BackgroundImagePlaceholder } from './background-image'
+import { LogoPlaceholder }            from './logo'
+
+export default {
+  title: 'Components/Placeholder',
+}
+
+export const Placeholder = ({ type, ...props }) => {
+  if (type === 'background-image') {
+    return (
+      <Box position='absolute' top={0} right={0} bottom={0} left={0}>
+        <BackgroundImagePlaceholder />
+      </Box>
+    )
+  }
+
+  if (type === 'horizontal-logo') {
+    return <LogoPlaceholder {...props} />
+  }
+
+  if (type === 'vertical-logo') {
+    return <LogoPlaceholder type='vertical' {...props} />
+  }
+
+  if (type === 'image-box') {
+    return (
+      <Box width={320} height={160}>
+        <ImageBoxPlaceholder />
+      </Box>
+    )
+  }
+
+  return <ImagePlaceholder {...props} />
+}
+
+Placeholder.args = {
+  type: 'image',
+}
+
+Placeholder.argTypes = {
+  type: {
+    name: 'Тип',
+    description: 'Тип плейсхолдера',
+    control: {
+      type: 'radio',
+      options: ['image', 'image-box', 'background-image', 'horizontal-logo', 'vertical-logo'],
+    },
+    table: {
+      category: 'Варианты',
+    },
+  },
+}
