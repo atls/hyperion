2aeec79af92c3dde7dabd9b33bb445163663f518
Nikita Elfimov <ne@atls.team>
2021-09-24 20:12:50 +0000

    fix(admin): upload fixes

--- ui-admin/upload/src/dropzone-placeholder.component.tsx
+++ ui-admin/upload/src/dropzone-placeholder.component.tsx
@@ -1,8 +1,6 @@
 import React                 from 'react'
 import styled                from '@emotion/styled'
 
-import { UploadIcon }        from '@admui/icons'
-
 import { PlaceholderAccept } from './placeholder-accept'
 import { PlaceholderIcon }   from './placeholder-icon'
 import { PlaceholderLink }   from './placeholder-link'
@@ -16,7 +14,8 @@ const Container = styled.div({
 const DropzonePlaceholder = ({ accept, children }: any) => (
   <Container>
     <PlaceholderIcon>
-      <UploadIcon height={40} width={40} />
+      {/* TODO use icon  */}
+      Icon
     </PlaceholderIcon>
     <PlaceholderAccept>{accept}</PlaceholderAccept>
     <PlaceholderLink>{children}</PlaceholderLink>
