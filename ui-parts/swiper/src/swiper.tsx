e52971fe10eeb8f61cf8a9f2d6f0cf3263f9c965
Nikita Elfimov <me@tfk.name>
2022-07-20 19:24:04 +0500

    fix(swiper): max-width injection

--- ui-parts/swiper/src/swiper.tsx
+++ ui-parts/swiper/src/swiper.tsx
@@ -3,18 +3,41 @@ import { injectGlobal }         from '@emotion/css'
 import React                    from 'react'
 import { Swiper as BaseSwiper } from 'swiper/react'
 import { SwiperProps }          from 'swiper/react'
+import { nanoid }               from 'nanoid'
+import { useMemo }              from 'react'
 
 import { STYLES }               from './swiper.styles'
 
-let wasInjected = false
+let wasGloballyInjected = false
+const injectedClasses: Array<string> = []
 
-const Swiper = (props: SwiperProps) => {
-  if (!wasInjected) {
+const Swiper = ({ width, className, ...props }: SwiperProps) => {
+  // eslint-disable-next-line
+  const cn = useMemo(() => className || `swiper-${nanoid()}`, [])
+
+  if (!wasGloballyInjected) {
     injectGlobal(STYLES)
-    wasInjected = true
+    injectGlobal(`.swiper {
+      display: flex;
+    }`)
+    wasGloballyInjected = true
   }
 
-  return <BaseSwiper {...props} />
+  if (!injectedClasses.includes(cn)) {
+    if (width) {
+      injectGlobal(`.${cn} {
+        max-width: ${width}px;
+      }`)
+    } else {
+      injectGlobal(`.${cn} {
+        max-width: 100vw;
+      }`)
+    }
+
+    injectedClasses.push(cn)
+  }
+
+  return <BaseSwiper width={width} className={cn} {...props} />
 }
 
 export * from 'swiper/react'
