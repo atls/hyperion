70ac05c05bd0350b1c51c7ac0568fa879a38a8a7
Nikita Elfimov <me@tfk.name>
2022-10-12 16:45:26 +0500

    feat(generators): executable for button generator

--- ui-proto/theme/src/theme/colors.ts
+++ ui-proto/theme/src/theme/colors.ts
@@ -3,4 +3,50 @@ export const colors = {
     primary: 'rgb(0, 0, 0, 1)',
     additional1: 'rgba(160,164,178,1)',
   },
+  button: {
+    primary: {
+      default: {
+        background: 'black',
+        font: 'white',
+        border: 'black',
+      },
+      hover: {
+        background: 'white',
+        font: 'black',
+        border: 'white',
+      },
+      pressed: {
+        background: 'white',
+        font: 'black',
+        border: 'white',
+      },
+      disabled: {
+        background: 'gray',
+        font: 'white',
+        border: 'gray',
+      },
+    },
+    secondary: {
+      default: {
+        background: 'white',
+        font: 'black',
+        border: 'white',
+      },
+      hover: {
+        background: 'black',
+        font: 'white',
+        border: 'black',
+      },
+      pressed: {
+        background: 'black',
+        font: 'white',
+        border: 'black',
+      },
+      disabled: {
+        background: 'gray',
+        font: 'white',
+        border: 'gray',
+      },
+    },
+  },
 }
