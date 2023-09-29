d1e25d229b6d447a9f8bd6177e88ef6f75934921
Nikita Elfimov <nelfimov@mail.ru>
2023-09-29 16:11:04 +0300

    chore: trigger release

--- ui-admin/button/src/button.component.tsx
+++ ui-admin/button/src/button.component.tsx
@@ -1,12 +1,9 @@
+import { Content }           from '@atls-ui-parts/button'
+import { ButtonProps }       from '@atls-ui-parts/button'
 import styled                from '@emotion/styled'
-
 import React                 from 'react'
 import { FC }                from 'react'
 import { PropsWithChildren } from 'react'
-
-import { Content }           from '@atls-ui-parts/button'
-import { ButtonProps }       from '@atls-ui-parts/button'
-
 import { baseStyles }        from './button.styles'
 import { shapeStyles }       from './button.styles'
 import { contentStyles }     from './button.styles'
