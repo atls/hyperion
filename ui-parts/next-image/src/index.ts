58c001a0fd0a2cf648b725d340db5447663eee99
Oksana Zhdanovich <oksss.vv@gmail.com>
2024-02-06 17:06:13 +0300

    feat: interface & required props

--- ui-parts/next-image/src/index.ts
+++ ui-parts/next-image/src/index.ts
@@ -1,2 +1,3 @@
 export * from './next-image.component'
 export * from './next-image.styles'
+export * from './next-image.interfaces'
---
+++ ui-parts/next-image/src/next-image.interfaces.ts
@@ -0,0 +1,6 @@
+export interface NextImageProps {
+  src: string
+  alt: string
+  width: number
+  height: number
+}
