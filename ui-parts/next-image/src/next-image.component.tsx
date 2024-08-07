445e66091caf3411d5a63e144c4efdd4f1c875ce
Dmitry <dimafomin02@gmail.com>
2024-08-07 06:07:02 +0300

    feat: updated @atls-ui-parts/next-image

--- ui-parts/next-image/src/next-image.component.tsx
+++ ui-parts/next-image/src/next-image.component.tsx
@@ -1,12 +1,16 @@
-import Image                from 'next/image'
-import React                from 'react'
-import { FC }               from 'react'
+import Image                      from 'next/image.js'
+import React                      from 'react'
+import { clsx }                   from 'clsx'
+import { forwardRef }             from 'react'
 
-import { NextImageProps }   from './next-image.interfaces'
-import { NextImageWrapper } from './next-image.styles'
+import { NextImageProps }         from './next-image.interfaces.js'
+import { nextImageWrapperStyles } from './next-image.css.js'
 
-export const NextImage: FC<NextImageProps> = ({ src, alt, width, height, ...props }) => (
-  <NextImageWrapper>
-    <Image src={src} alt={alt} width={width} height={height} {...props} />
-  </NextImageWrapper>
-)
+export const NextImage = forwardRef<HTMLImageElement, NextImageProps>((
+  { wrapperClassName, ...props },
+  ref
+) => (
+  <div className={clsx(nextImageWrapperStyles, wrapperClassName)}>
+    <Image.default ref={ref} {...props} />
+  </div>
+))
