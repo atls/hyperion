d8e9c684f37f181d8e939cee0785e859d78c7ce0
Dmitry <dimafomin02@gmail.com>
2024-08-09 07:23:08 +0300

    feat: updated @atls-ui-admin/button

--- ui-parts/button/src/index.ts
+++ ui-parts/button/src/index.ts
@@ -1,4 +1,5 @@
 export * from './button.component.js'
+export * from './button.interfaces.js'
 export * from './content/index.js'
 export * from './ghost-button/index.js'
 export * from './utils/index.js'
