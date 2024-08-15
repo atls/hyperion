4b36fcbce73c82a6ad141a342048b6361d0bd4dc
Dmitry <dimafomin02@gmail.com>
2024-08-15 21:06:35 +0300

    refactor: changed @atls-ui-parts/image

--- ui-parts/image/src/image.component.tsx
+++ ui-parts/image/src/image.component.tsx
@@ -4,25 +4,21 @@ import { ImgHTMLAttributes } from 'react'
 import { clsx }              from 'clsx'
 import { forwardRef }        from 'react'
 
-import { baseImageStyles }   from './image.styles.css.js'
-import { imageSprinkles }    from './image.styles.css.js'
+import { baseImageStyles }   from './image.css.js'
+import { imageSprinkles }    from './image.css.js'
 
 interface ImageProps extends ImgHTMLAttributes<HTMLImageElement> {}
 
 export const Image = forwardRef<HTMLImageElement, PropsWithChildren<ImageProps>>((
-  { alt, className, width, height, ...props },
+  { alt, width, height, ...props },
   ref
 ) => {
-  const { style, otherProps } = imageSprinkles({
-    ...props,
-    width,
-    height,
-  })
+  const { className, style, otherProps } = imageSprinkles({ ...props, width, height })
 
   return (
     <img
       ref={ref}
-      className={clsx(baseImageStyles, className)}
+      className={clsx(baseImageStyles, className, otherProps?.className)}
       alt={alt}
       {...otherProps}
       style={{ ...style, ...otherProps?.style }}
