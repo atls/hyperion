dcc201985dfabff296e442ed47c06e30c1624ad3
Dmitry <dimafomin02@gmail.com>
2024-08-15 21:08:53 +0300

    refactor: changed @atls-ui-parts/sidebar structure

--- ui-parts/sidebar/src/sidebar.stories.tsx
+++ ui-parts/sidebar/src/sidebar.stories.tsx
@@ -1,35 +1,29 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Meta }            from '@storybook/react'
-import { StoryObj }        from '@storybook/react'
-import { faCog }           from '@fortawesome/free-solid-svg-icons'
+import { Meta }      from '@storybook/react'
+import { StoryObj }  from '@storybook/react'
 
-import React               from 'react'
+import React         from 'react'
 
-import { Column }          from '@atls-ui-parts/layout'
-import { Layout }          from '@atls-ui-parts/layout'
+import { Column }    from '@atls-ui-parts/layout'
+import { Layout }    from '@atls-ui-parts/layout'
 
-import { Item }            from './item/index.js'
-import { ItemIcon }        from './item/index.js'
-import { ItemLabel }       from './item/index.js'
-import { Sidebar }         from './sidebar.component.js'
+import { Item }      from './item/index.js'
+import { ItemIcon }  from './item/index.js'
+import { ItemLabel } from './item/index.js'
+import { Sidebar }   from './sidebar.component.js'
 
 const meta: Meta = {
   title: 'Components/Sidebar',
   render: () => (
-    <Column fill alignItems='center' justifyContent='center'>
+    <Column fill justifyContent='center'>
       <Layout flexBasis='50px' />
       <Sidebar>
-        <Item active href='/'>
-          <ItemIcon>
-            <FontAwesomeIcon icon={faCog} />
-          </ItemIcon>
-          <ItemLabel>Test text</ItemLabel>
+        <Item active>
+          <ItemIcon>⚙️</ItemIcon>
+          <ItemLabel>Settings</ItemLabel>
         </Item>
-        <Item active={false} href='/'>
-          <ItemIcon>
-            <FontAwesomeIcon icon={faCog} />
-          </ItemIcon>
-          <ItemLabel>Test text</ItemLabel>
+        <Item active={false}>
+          <ItemIcon>🍕</ItemIcon>
+          <ItemLabel>Pizza</ItemLabel>
         </Item>
       </Sidebar>
       <Layout flexBasis='50px' />
