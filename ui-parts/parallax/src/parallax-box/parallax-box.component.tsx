934aa7bd5fc6da3081cf74f0be05a134ffd5210a
Dmitry <dimafomin02@gmail.com>
2024-08-22 01:57:03 +0300

    feat: added non themed parallax-box  element to @atls-ui-parts/parallax

--- ui-parts/parallax/src/parallax-box/parallax-box.component.tsx
+++ ui-parts/parallax/src/parallax-box/parallax-box.component.tsx
@@ -1,71 +1,18 @@
-import React                   from 'react'
-import { MotionValue }         from 'framer-motion'
-import { clsx }                from 'clsx'
-import { motion }              from 'framer-motion'
-import { forwardRef }          from 'react'
-import { useMemo }             from 'react'
+import React                  from 'react'
+import { forwardRef }         from 'react'
 
-import { ParallaxBoxProps }    from './parallax-box.interfaces.js'
-import { useParallax }         from '../context/index.js'
-import { parallaxSprinkles }   from './parallax-box.css.js'
-import { getTransform }        from './parallax-box.utils.js'
-import { getTransformDisplay } from './parallax-box.utils.js'
+import { ParallaxBoxElement } from './parallax-box.element.js'
+import { ParallaxBoxProps }   from './parallax-box.interfaces.js'
+import { parallaxSprinkles }  from './parallax-box.css.js'
 
 export const ParallaxBox = forwardRef<HTMLDivElement, ParallaxBoxProps>((
-  {
-    children,
-    inputRange,
-    pageNumber = 0,
-    animations = {},
-    ease = 'easeInOut',
-    heightMultiplier = 1,
-    ...props
-  },
+  { children, ...props },
   ref
-) => {
-  const { className, style, otherProps } = parallaxSprinkles(props)
-
-  const [scrollY, windowHeight] = useParallax()
-
-  const range = useMemo(
-    () =>
-      inputRange.map(
-        (item) => item * heightMultiplier * Number(windowHeight) + Number(windowHeight) * pageNumber
-      ),
-    [windowHeight, inputRange, pageNumber, heightMultiplier]
-  )
-
-  const animatedStyles = useMemo<Record<string, MotionValue<string | number>>>(() => {
-    const values: Record<string, MotionValue<string | number>> = {}
-
-    if (!scrollY) return values
-
-    Object.keys(animations).forEach((key: string) => {
-      values[key] = getTransform(scrollY, range, animations[key], ease)
-    })
-
-    return values
-  }, [animations, scrollY, range, ease])
-
-  const displayStyle = useMemo(() => {
-    if (!animatedStyles?.display) return {}
-
-    return {
-      display: getTransformDisplay(animatedStyles.display),
-    }
-  }, [animatedStyles])
-
-  return (
-    <motion.div
-      ref={ref}
-      {...otherProps}
-      className={clsx(className, otherProps?.className)}
-      style={{ ...style, ...otherProps?.style, ...animatedStyles, ...displayStyle }}
-    >
-      {children}
-    </motion.div>
-  )
-})
+) => (
+  <ParallaxBoxElement ref={ref} sprinkles={parallaxSprinkles as any} {...props}>
+    {children}
+  </ParallaxBoxElement>
+))
 
 ParallaxBox.defaultProps = {
   display: 'flex',
--- ui-parts/parallax/src/parallax-box/parallax-box.interfaces.ts
+++ ui-parts/parallax/src/parallax-box/parallax-box.interfaces.ts
@@ -1,6 +1,7 @@
-import { HTMLMotionProps }   from 'framer-motion'
+import { HTMLMotionProps }        from 'framer-motion'
+import { createRainbowSprinkles } from 'rainbow-sprinkles'
 
-import { ParallaxSprinkles } from './parallax-box.css.js'
+import { ParallaxSprinkles }      from './parallax-box.css.js'
 
 export type ParallaxEaseType = 'linear' | 'easeInOut'
 
@@ -8,10 +9,16 @@ export type ParallaxAnimationsType = {
   [key: string]: Array<number | string>
 }
 
-export interface ParallaxBoxProps extends HTMLMotionProps<'div'>, ParallaxSprinkles {
+export interface BaseParallaxBoxProps extends HTMLMotionProps<'div'> {
   inputRange: number[]
   animations?: ParallaxAnimationsType
   pageNumber?: number
   ease?: ParallaxEaseType
   heightMultiplier?: number
 }
+
+export interface ParallaxBoxElementProps extends BaseParallaxBoxProps {
+  sprinkles: ReturnType<typeof createRainbowSprinkles>
+}
+
+export interface ParallaxBoxProps extends BaseParallaxBoxProps, ParallaxSprinkles {}
