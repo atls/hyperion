a25cb8256c0243b84188e568556fbc67a1c2bc77
Dmitry <dimafomin02@gmail.com>
2024-08-28 13:11:11 +0300

    fix: @atls-ui-parts/design build

--- ui-parts/design/src/index.ts
+++ ui-parts/design/src/index.ts
@@ -1,15 +1,9 @@
 import express from 'express'
-import cors from 'cors'
 
 const app = express()
+
 const port = 3000
 
-app.use(
-  cors({
-    credentials: true,
-    origin: true,
-  })
-)
 app.use(express.static('dist'))
 
 app.listen(port, () => {
