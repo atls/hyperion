7d2b1d12c897399d6214597218892efa41dc2ffd
Dmitry Fomin <dimafomin02@gmail.com>
2025-10-29 05:43:54 +0300

    feat(generators): add components generator

---
+++ generators/components/src/config.ts
@@ -0,0 +1,31 @@
+import type { NodePlopAPI } from 'plop'
+
+import { join }             from 'node:path'
+
+const generator = (plop: NodePlopAPI): void => {
+  plop.setGenerator('component', {
+    description: 'Create a components',
+    prompts: [
+      {
+        type: 'list',
+        name: 'type',
+        message: 'Select component type:',
+        choices: [
+          { name: 'Popover', value: 'popover' },
+          { name: 'Tooltip', value: 'tooltip' },
+        ],
+      },
+    ],
+    actions: (data) => [
+      {
+        type: 'addMany',
+        base: `./templates/${data?.type}`,
+        destination: join(process.cwd(), 'src'),
+        templateFiles: `./templates/${data?.type}/**/*`,
+        force: true,
+      },
+    ],
+  })
+}
+
+export default generator
