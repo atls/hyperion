f8ce1b8098413c24148a3dce4c616c0078b08db1
Andrew Ghostuhin <me@torinasakura.name>
2025-11-10 01:55:10 +0300

    fix(ui-admin): bottom navigation item colors

--- ui-admin/bottom-navigation/src/bottom-navigation-item/bottom-navigation-item.component.tsx
+++ ui-admin/bottom-navigation/src/bottom-navigation-item/bottom-navigation-item.component.tsx
@@ -4,6 +4,8 @@ import type { ReactElement }          from 'react'
 import { createElement }              from 'react'
 import React                          from 'react'
 
+import { vars }                       from '@atls-ui-admin/theme'
+
 import { bottomNavigationItemStyles } from './bottom-navigation-item.styles.css.js'
 
 export interface BottomNavigationItemProps {
@@ -17,7 +19,7 @@ export interface BottomNavigationItemProps {
 export const BottomNavigationItem = ({ icon }: BottomNavigationItemProps): ReactElement => (
   <div className={bottomNavigationItemStyles}>
     {createElement(icon, {
-      color: 'white',
+      color: vars.colors.white,
       width: 20,
       height: 20,
     })}
