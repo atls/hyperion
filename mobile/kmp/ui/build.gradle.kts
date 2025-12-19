7a6ef748a41c03cd1c3976d40fc3e8f60193b86d
Arina Gazhina <gazhina@internet.ru>
2025-12-19 23:55:58 +0300

    feat(mobile): ui library module

---
+++ mobile/kmp/ui/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+}
+
+kotlin {
+    androidTarget {
+        publishLibraryVariants("release", "debug")
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "21"
+            }
+        }
+    }
+    
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "ui"
+            isStatic = true
+        }
+    }
+    
+    sourceSets {
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material)
+            implementation(compose.ui)
+            implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
+        }
+    }
+}
+
+android {
+    namespace = "com.atls.hyperion.ui"
+    compileSdk = 36
+    defaultConfig {
+        minSdk = 24
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+}
---
+++ mobile/kmp/ui/src/commonMain/kotlin/com/atls/hyperion/ui/Placeholder.kt
@@ -0,0 +1,3 @@
+package com.atls.hyperion.ui
+
+//TODO remove after implementing ui
