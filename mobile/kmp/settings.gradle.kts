cb6b312e3b9f9d66da595c6752751cf5417f81a6
Arina Gazhina <gazhina@internet.ru>
2025-12-19 23:54:27 +0300

    feat(common): init gradle project

---
+++ mobile/kmp/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "hyperion"
+include(":sample")
+include(":ui")
