e423e91f6576b7e3c9d312cbfbd4a75756c7b54e
Arina Gazhina <gazhina@internet.ru>
2026-01-09 19:53:55 +0300

    feat(typography): font properties

---
+++ mobile/kmp/ui/src/commonMain/kotlin/com/atls/hyperion/ui/typography/FontSizes.kt
@@ -0,0 +1,37 @@
+package com.atls.hyperion.ui.typography
+
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
+
+object FontSize {
+    val unspecified = TextUnit.Unspecified
+    val semiTiny = 6.sp
+    val semiSmall = 8.sp
+    val small = 9.sp
+    val tiny = 10.sp
+    val semiMicro = 11.sp
+    val micro = 12.sp
+    val semiBold = 14.sp
+    val middling = 15.sp
+    val medium = 16.sp
+    val semiRegular = 18.sp
+    val regular = 20.sp
+    val semiLarge = 22.sp
+    val large = 24.sp
+    val semiModerate = 28.sp
+    val moderate = 32.sp
+    val mild = 36.sp
+    val semiIncreased = 38.sp
+    val increased = 40.sp
+    val semiBig = 44.sp
+    val big = 48.sp
+    val semiExtra = 56.sp
+    val extra = 60.sp
+    val semiMassive = 64.sp
+    val massive = 68.sp
+    val godzilla = 70.sp
+    val semiHuge = 76.sp
+    val huge = 80.sp
+    val semiGiant = 86.sp
+    val semiGigantic = 103.sp
+}
---
+++ mobile/kmp/ui/src/commonMain/kotlin/com/atls/hyperion/ui/typography/FontWeights.kt
@@ -0,0 +1,10 @@
+package com.atls.hyperion.ui.typography
+
+import androidx.compose.ui.text.font.FontWeight
+
+object FontWeight {
+    val light = FontWeight.W300
+    val regular = FontWeight.W400
+    val medium = FontWeight.W500
+    val bold = FontWeight.W600
+}
