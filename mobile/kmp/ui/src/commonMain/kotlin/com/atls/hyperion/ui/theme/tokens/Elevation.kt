ce06810f7f4664c98d5e19ba4f9906d51746edd3
Arina Gazhina <gazhina@internet.ru>
2026-01-10 02:31:18 +0300

    chore(theme): remove unused elevation

--- mobile/kmp/ui/src/commonMain/kotlin/com/atls/hyperion/ui/theme/tokens/Elevation.kt
+++ mobile/kmp/ui/src/commonMain/kotlin/com/atls/hyperion/ui/theme/tokens/Elevation.kt
@@ -3,9 +3,5 @@ package com.atls.hyperion.ui.theme.tokens
 import androidx.compose.ui.unit.dp
 
 object Elevation {
-    val none = 0.dp
-    val micro = 2.dp
-    val tiny = 6.dp
-    val small = 12.dp
-    val large = 20.dp
+    val zero = 0.dp
 }
