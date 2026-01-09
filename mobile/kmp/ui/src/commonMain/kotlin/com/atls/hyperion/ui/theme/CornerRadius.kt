4451e47ab42abe95a47aa9dda465ed99479e9117
Arina Gazhina <gazhina@internet.ru>
2026-01-09 18:17:00 +0300

    feat(theme): radius

---
+++ mobile/kmp/ui/src/commonMain/kotlin/com/atls/hyperion/ui/theme/CornerRadius.kt
@@ -0,0 +1,48 @@
+package com.atls.hyperion.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object CornerRadius {
+    val none = 0.dp
+    val full = 100.dp
+    val f1 = 1.dp
+    val f2 = 2.dp
+    val f3 = 3.dp
+    val f4 = 4.dp
+    val f5 = 5.dp
+    val f6 = 6.dp
+    val f7 = 7.dp
+    val f8 = 8.dp
+    val f10 = 10.dp
+    val f11 = 11.dp
+    val f12 = 12.dp
+    val f14 = 14.dp
+    val f15 = 15.dp
+    val f16 = 16.dp
+    val f18 = 18.dp
+    val f19 = 19.dp
+    val f20 = 20.dp
+    val f22 = 22.dp
+    val f24 = 24.dp
+    val f27 = 27.dp
+    val f29 = 29.dp
+    val f32 = 32.dp
+    val f34 = 34.dp
+    val f40 = 40.dp
+    val f50 = 50.dp
+    val f60 = 60.dp
+    val f80 = 80.dp
+    val f100 = 100.dp
+    val f125 = 125.dp
+    val f140 = 140.dp
+    val f160 = 160.dp
+    val f178 = 178.dp
+    val f222 = 222.dp
+    val f234 = 234.dp
+    val f240 = 240.dp
+    val f278 = 278.dp
+    val f315 = 315.dp
+    val f500 = 500.dp
+    val f600 = 600.dp
+    val f9999 = 9999.dp
+}
