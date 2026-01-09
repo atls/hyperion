23d8f2d5b31d7c7d10be4522f1fb4673f916baad
Arina Gazhina <gazhina@internet.ru>
2026-01-10 02:29:15 +0300

    refactor(button): state as enum

--- mobile/kmp/ui/src/commonMain/kotlin/com/atls/hyperion/ui/components/button/state/ButtonState.kt
+++ mobile/kmp/ui/src/commonMain/kotlin/com/atls/hyperion/ui/components/button/state/ButtonState.kt
@@ -1,7 +1,7 @@
 package com.atls.hyperion.ui.components.button.state
 
-sealed interface ButtonState {
-    object Default : ButtonState
-    object Disabled : ButtonState
-    object Pressed : ButtonState
+enum class ButtonState {
+    Default,
+    Disabled,
+    Pressed
 }
