11b4d5c79d63cf30f1eb701ee572ba2c4d3bc676
Arina Gazhina <gazhina@internet.ru>
2026-01-10 01:16:54 +0300

    refactor(storybook): module structure

--- mobile/kmp/sample/src/commonMain/kotlin/com/atls/hyperion/sample/App.kt
+++ mobile/kmp/sample/src/commonMain/kotlin/com/atls/hyperion/sample/App.kt
@@ -2,7 +2,7 @@ package com.atls.hyperion.sample
 
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import com.atls.hyperion.storybook.Storybook
+import com.atls.hyperion.storybook.fragments.storybook.Storybook
 import com.atls.hyperion.ui.components.button.stories.ButtonExample
 
 @Composable
