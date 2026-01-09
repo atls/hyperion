a07fd7f7582fbe7a2648aea7f04b9b0e12eabfee
Arina Gazhina <gazhina@internet.ru>
2026-01-09 18:29:58 +0300

    feat(theme): spaces

---
+++ mobile/kmp/ui/src/commonMain/kotlin/com/atls/hyperion/ui/theme/Space.kt
@@ -0,0 +1,17 @@
+package com.atls.hyperion.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object Space {
+    val zero = 0.dp
+    val g1 = 1.dp
+    val g4 = 4.dp
+    val g6 = 6.dp
+    val g8 = 8.dp
+    val g10 = 10.dp
+    val g12 = 12.dp
+    val g14 = 14.dp
+    val g17 = 17.dp
+    val g22 = 22.dp
+    val g24 = 24.dp
+}
