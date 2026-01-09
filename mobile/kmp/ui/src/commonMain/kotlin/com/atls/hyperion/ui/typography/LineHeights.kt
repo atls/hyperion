d724e585546de490f98c047707b715ce58d70e78
Arina Gazhina <gazhina@internet.ru>
2026-01-09 18:30:49 +0300

    feat(ui): line heights

---
+++ mobile/kmp/ui/src/commonMain/kotlin/com/atls/hyperion/ui/typography/LineHeights.kt
@@ -0,0 +1,19 @@
+package com.atls.hyperion.ui.typography
+
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
+
+object LineHeights {
+    val micro = 0.5.sp
+    val extraMicro = 0.8.sp
+    val small = 0.9.sp
+    val compact = 1.sp
+    val extraCompact = 1.1.sp
+    val default = 1.2.sp
+    val extraDefault = 1.3.sp
+    val medium = 1.4.sp
+    val extraMedium = 1.5.sp
+    val increased = 1.6.sp
+    val extraIncreased = 1.8.sp
+    val big = 2.3.sp
+}
