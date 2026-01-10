d04a833915ab0a1274653c26c6c6ee2ff55f7b31
Arina Gazhina <gazhina@internet.ru>
2026-01-10 04:54:36 +0300

    feat(theme): checkbox and image sizes

---
+++ mobile/kmp/ui/src/commonMain/kotlin/com/atls/hyperion/ui/theme/tokens/components/CheckboxSize.kt
@@ -0,0 +1,9 @@
+package com.atls.hyperion.ui.theme.tokens.components
+
+import androidx.compose.ui.unit.dp
+
+object CheckboxSize {
+    val large = 32.dp
+    val medium = 24.dp
+    val small = 16.dp
+}
---
+++ mobile/kmp/ui/src/commonMain/kotlin/com/atls/hyperion/ui/theme/tokens/components/ImageSize.kt
@@ -0,0 +1,9 @@
+package com.atls.hyperion.ui.theme.tokens.components
+
+import androidx.compose.ui.unit.dp
+
+object ImageSize {
+    val huge = 58.dp
+    val large = 48.dp
+    val medium = 40.dp
+}
