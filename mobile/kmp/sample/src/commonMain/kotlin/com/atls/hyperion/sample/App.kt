5644f2ad270830131428b1b2a9bcce06d13fc88b
Arina Gazhina <gazhina@internet.ru>
2026-01-12 22:38:31 +0300

    chore(sample): new components story usage

--- mobile/kmp/sample/src/commonMain/kotlin/com/atls/hyperion/sample/App.kt
+++ mobile/kmp/sample/src/commonMain/kotlin/com/atls/hyperion/sample/App.kt
@@ -3,15 +3,30 @@ package com.atls.hyperion.sample
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import com.atls.hyperion.storybook.fragments.storybook.Storybook
-import com.atls.hyperion.ui.components.button.stories.ButtonExample
+import com.atls.hyperion.ui.components.avatar.stories.AvatarStory
+import com.atls.hyperion.ui.components.button.stories.ButtonStory
+import com.atls.hyperion.ui.components.card.stories.CardStory
+import com.atls.hyperion.ui.components.checkbox.stories.CheckboxStory
+import com.atls.hyperion.ui.components.divider.stories.DividerStory
+import com.atls.hyperion.ui.components.input.stories.InputStory
+import com.atls.hyperion.ui.components.switch.stories.SwitchStory
+import com.atls.hyperion.ui.primitives.stories.LinkStory
+import com.atls.hyperion.ui.primitives.stories.TextStory
 
 @Composable
 fun App() {
     MaterialTheme {
         Storybook(
             components = listOf(
-                ButtonExample(),
-                ButtonExample()
+                AvatarStory(),
+                ButtonStory(),
+                CheckboxStory(),
+                DividerStory(),
+                InputStory(),
+                SwitchStory(),
+                CardStory(),
+                TextStory(),
+                LinkStory()
             )
         )
     }
