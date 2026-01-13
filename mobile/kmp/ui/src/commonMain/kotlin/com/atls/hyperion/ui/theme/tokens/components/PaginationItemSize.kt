4f93fdc267bdbe3960f3ec8580235b220a87f8cc
Arina Gazhina <gazhina@internet.ru>
2026-01-13 14:15:11 +0300

    feat(theme): pagination item sizes

---
+++ mobile/kmp/ui/src/commonMain/kotlin/com/atls/hyperion/ui/theme/tokens/components/PaginationItemSize.kt
@@ -0,0 +1,11 @@
+package com.atls.hyperion.ui.theme.tokens.components
+
+import androidx.compose.ui.unit.dp
+
+object PaginationItemSize {
+    val medium = 12.dp
+    val small = 8.dp
+
+    val rectangleHeight = 4.dp
+    val rectangleWidth = 20.dp
+}
