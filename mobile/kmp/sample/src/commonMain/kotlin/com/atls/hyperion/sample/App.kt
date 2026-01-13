5afab2a205275ff279ec068dd9fec795f8acb18e
Arina Gazhina <gazhina@internet.ru>
2026-01-13 20:28:58 +0300

    feat(app): use new stories

--- mobile/kmp/sample/src/commonMain/kotlin/com/atls/hyperion/sample/App.kt
+++ mobile/kmp/sample/src/commonMain/kotlin/com/atls/hyperion/sample/App.kt
@@ -4,6 +4,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import com.atls.hyperion.storybook.fragments.storybook.Storybook
 import com.atls.hyperion.ui.components.avatar.stories.AvatarStory
+import com.atls.hyperion.ui.components.bottombar.stories.BottomBarStory
 import com.atls.hyperion.ui.components.button.stories.ButtonStory
 import com.atls.hyperion.ui.components.card.stories.CardStory
 import com.atls.hyperion.ui.components.carousel.stories.CarouselStory
@@ -13,9 +14,11 @@ import com.atls.hyperion.ui.components.input.stories.InputStory
 import com.atls.hyperion.ui.components.modal.bottom.stories.BottomDialogStory
 import com.atls.hyperion.ui.components.modal.popup.stories.PopupStory
 import com.atls.hyperion.ui.components.pagination.stories.PaginationStory
+import com.atls.hyperion.ui.components.placeholder.stories.PlaceholderStory
+import com.atls.hyperion.ui.components.progress.stories.ProgressStory
 import com.atls.hyperion.ui.components.switch.stories.SwitchStory
-import com.atls.hyperion.ui.fragment.datepicker.stories.DatePickerStory
-import com.atls.hyperion.ui.fragment.datepicker.stories.DateRangePickerStory
+import com.atls.hyperion.ui.fragments.datepicker.stories.DatePickerStory
+import com.atls.hyperion.ui.fragments.datepicker.stories.DateRangePickerStory
 import com.atls.hyperion.ui.primitives.stories.LinkStory
 import com.atls.hyperion.ui.primitives.stories.TextStory
 
@@ -26,6 +29,7 @@ fun App() {
             components = listOf(
                 AvatarStory(),
                 BottomDialogStory(),
+                BottomBarStory(),
                 ButtonStory(),
                 CardStory(),
                 CarouselStory(),
@@ -35,7 +39,9 @@ fun App() {
                 DividerStory(),
                 InputStory(),
                 PaginationStory(),
+                PlaceholderStory(),
                 PopupStory(),
+                ProgressStory(),
                 SwitchStory(),
                 TextStory(),
                 LinkStory()
