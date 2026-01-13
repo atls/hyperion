628a1c739bea7a464431037ec4c4226368210d3f
Arina Gazhina <gazhina@internet.ru>
2026-01-13 14:23:29 +0300

    feat(ui): pagination story

--- mobile/kmp/sample/src/commonMain/kotlin/com/atls/hyperion/sample/App.kt
+++ mobile/kmp/sample/src/commonMain/kotlin/com/atls/hyperion/sample/App.kt
@@ -6,11 +6,13 @@ import com.atls.hyperion.storybook.fragments.storybook.Storybook
 import com.atls.hyperion.ui.components.avatar.stories.AvatarStory
 import com.atls.hyperion.ui.components.button.stories.ButtonStory
 import com.atls.hyperion.ui.components.card.stories.CardStory
+import com.atls.hyperion.ui.components.carousel.stories.CarouselStory
 import com.atls.hyperion.ui.components.checkbox.stories.CheckboxStory
 import com.atls.hyperion.ui.components.divider.stories.DividerStory
 import com.atls.hyperion.ui.components.input.stories.InputStory
 import com.atls.hyperion.ui.components.modal.bottom.stories.BottomDialogStory
 import com.atls.hyperion.ui.components.modal.popup.stories.PopupStory
+import com.atls.hyperion.ui.components.pagination.stories.PaginationStory
 import com.atls.hyperion.ui.components.switch.stories.SwitchStory
 import com.atls.hyperion.ui.fragment.datepicker.stories.DatePickerStory
 import com.atls.hyperion.ui.fragment.datepicker.stories.DateRangePickerStory
@@ -25,14 +27,16 @@ fun App() {
                 AvatarStory(),
                 BottomDialogStory(),
                 ButtonStory(),
+                CardStory(),
+                CarouselStory(),
                 CheckboxStory(),
                 DatePickerStory(),
                 DateRangePickerStory(),
                 DividerStory(),
                 InputStory(),
+                PaginationStory(),
                 PopupStory(),
                 SwitchStory(),
-                CardStory(),
                 TextStory(),
                 LinkStory()
             )
