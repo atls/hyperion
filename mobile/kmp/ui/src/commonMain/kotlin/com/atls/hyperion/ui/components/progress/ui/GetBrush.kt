0d9c804c2817335fde2e0ade97c76905e88d6fbd
Arina Gazhina <gazhina@internet.ru>
2026-01-13 19:42:31 +0300

    feat(ui): progress constants/utils

---
+++ mobile/kmp/ui/src/commonMain/kotlin/com/atls/hyperion/ui/components/progress/ui/GetBrush.kt
@@ -0,0 +1,19 @@
+package com.atls.hyperion.ui.components.progress.ui
+
+import androidx.compose.ui.graphics.Brush
+import com.atls.hyperion.ui.components.progress.styles.appearance.Colors
+import com.atls.hyperion.ui.theme.tokens.colors.Colors as ThemeColors
+
+fun getBrush(colors: Colors, index: Int): Brush? = when (colors) {
+    is Colors.Single.Gradient -> colors.strokeBrush
+    is Colors.Multiple.Gradient -> colors.strokeBrushes.getOrElse(index) {
+        Brush.linearGradient(
+            listOf(
+                ThemeColors.Palette.transparent,
+                ThemeColors.Palette.transparent
+            )
+        )
+    }
+
+    else -> null
+}
