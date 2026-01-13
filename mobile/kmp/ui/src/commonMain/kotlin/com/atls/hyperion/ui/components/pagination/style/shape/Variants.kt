768d874f28b4a61f0de10ddd1944abfaf9c10f50
Arina Gazhina <gazhina@internet.ru>
2026-01-13 14:14:44 +0300

    feat(ui): pagination styles

---
+++ mobile/kmp/ui/src/commonMain/kotlin/com/atls/hyperion/ui/components/pagination/style/shape/Variants.kt
@@ -0,0 +1,28 @@
+package com.atls.hyperion.ui.components.pagination.style.shape
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.DpSize
+import com.atls.hyperion.ui.theme.tokens.components.PaginationItemSize
+import com.atls.hyperion.ui.theme.tokens.layout.CornerRadius
+import com.atls.hyperion.ui.theme.tokens.layout.Space
+
+@Composable
+fun PaginationShape.Companion.circle(): PaginationShape =
+    PaginationShape(
+        activeSize = DpSize(PaginationItemSize.medium, PaginationItemSize.medium),
+        disabledSize = DpSize(PaginationItemSize.small, PaginationItemSize.small),
+        spacing = Space.g16,
+        cornerRadius = CornerRadius.full
+    )
+
+@Composable
+fun PaginationShape.Companion.rectangle(): PaginationShape =
+    PaginationShape(
+        activeSize = DpSize(PaginationItemSize.rectangleWidth, PaginationItemSize.rectangleHeight),
+        disabledSize = DpSize(
+            PaginationItemSize.rectangleWidth,
+            PaginationItemSize.rectangleHeight
+        ),
+        spacing = Space.g8,
+        cornerRadius = CornerRadius.md
+    )
