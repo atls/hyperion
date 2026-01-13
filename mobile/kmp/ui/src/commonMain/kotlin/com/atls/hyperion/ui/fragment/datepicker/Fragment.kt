12d5782feb1a80be5731f9e2b0e973f9e0a18f71
Arina Gazhina <gazhina@internet.ru>
2026-01-13 04:52:54 +0300

    feat(ui): date picker fragment

---
+++ mobile/kmp/ui/src/commonMain/kotlin/com/atls/hyperion/ui/fragment/datepicker/Fragment.kt
@@ -0,0 +1,33 @@
+package com.atls.hyperion.ui.fragment.datepicker
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.atls.hyperion.ui.components.modal.popup.Popup
+import com.atls.hyperion.ui.fragment.datepicker.model.DateSelection
+import com.atls.hyperion.ui.fragment.datepicker.style.appearance.DatePickerAppearance
+import com.atls.hyperion.ui.fragment.datepicker.style.appearance.default
+import com.atls.hyperion.ui.fragment.datepicker.style.shape.DatePickerShape
+import com.atls.hyperion.ui.fragment.datepicker.style.shape.default
+import com.atls.hyperion.ui.fragment.datepicker.ui.DatePickerContent
+
+@Composable
+fun DatePicker(
+    modifier: Modifier = Modifier,
+    selection: DateSelection,
+    onSelectionChange: (DateSelection) -> Unit,
+    onDismiss: () -> Unit,
+    appearance: DatePickerAppearance = DatePickerAppearance.default(),
+    shape: DatePickerShape = DatePickerShape.default()
+) {
+    Popup(
+        modifier = modifier,
+        onDismissRequest = onDismiss
+    ) {
+        DatePickerContent(
+            selection = selection,
+            onSelectionChange = onSelectionChange,
+            appearance = appearance,
+            shape = shape
+        )
+    }
+}
