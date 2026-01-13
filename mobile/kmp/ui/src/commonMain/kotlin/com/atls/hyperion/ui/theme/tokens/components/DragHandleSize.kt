278ef5febd41e9dc6845326b41fa16e4654432a7
Arina Gazhina <gazhina@internet.ru>
2026-01-13 04:55:11 +0300

    feat(theme): draghandle sizes

---
+++ mobile/kmp/ui/src/commonMain/kotlin/com/atls/hyperion/ui/theme/tokens/components/DragHandleSize.kt
@@ -0,0 +1,9 @@
+package com.atls.hyperion.ui.theme.tokens.components
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+object DragHandleSize {
+    val height: Dp = 6.dp
+    val width: Dp = 79.dp
+}
