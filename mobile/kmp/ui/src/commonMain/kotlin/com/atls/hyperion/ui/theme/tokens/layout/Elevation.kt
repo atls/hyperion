78ee31f71b1f484f19d8bad649f6454a0b37acb5
Arina Gazhina <gazhina@internet.ru>
2026-08-18 22:26:08 +0300

    chore(theme): mark legacy elevation tokens

--- mobile/kmp/ui/src/commonMain/kotlin/com/atls/hyperion/ui/theme/tokens/layout/Elevation.kt
+++ mobile/kmp/ui/src/commonMain/kotlin/com/atls/hyperion/ui/theme/tokens/layout/Elevation.kt
@@ -2,7 +2,7 @@ package com.atls.hyperion.ui.theme.tokens.layout
 
 import androidx.compose.ui.unit.dp
 
-object Elevation {
+object Elevation { //TODO remove after components refactoring
     val zero = 0.dp
     val tiny = 2.dp
     val medium = 8.dp
