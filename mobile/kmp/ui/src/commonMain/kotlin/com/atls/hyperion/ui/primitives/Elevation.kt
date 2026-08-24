c6677f5ccd063ba62d3ab25ad5be9bb74ed8f378
Arina Gazhina <gazhina@internet.ru>
2026-08-25 01:24:26 +0300

    fix(theme): let gradient background passing in elevation

--- mobile/kmp/ui/src/commonMain/kotlin/com/atls/hyperion/ui/primitives/Elevation.kt
+++ mobile/kmp/ui/src/commonMain/kotlin/com/atls/hyperion/ui/primitives/Elevation.kt
@@ -4,9 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.dropShadow
 import androidx.compose.ui.draw.innerShadow
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.*
 import com.atls.hyperion.ui.primitives.utils.clearDropShadowInside
 import com.atls.hyperion.ui.primitives.utils.isolateDropShadows
 import com.atls.hyperion.ui.primitives.utils.toComposeShadow
@@ -17,12 +15,27 @@ fun Modifier.elevation(
     elevation: Elevation,
     backgroundColor: Color,
     shape: Shape = RectangleShape
+): Modifier = elevation(
+    elevation = elevation,
+    backgroundBrush = SolidColor(backgroundColor),
+    shape = shape
+)
+
+fun Modifier.elevation(
+    elevation: Elevation,
+    backgroundBrush: Brush,
+    shape: Shape = RectangleShape,
+    backgroundAlpha: Float = 1f
 ): Modifier {
+    val fillAlpha = backgroundAlpha * when (backgroundBrush) {
+        is SolidColor -> backgroundBrush.value.alpha
+        else -> 1f
+    }
     val dropShadows = elevation.shadows.filter { it.type == ShadowType.Drop }
     val withDropShadows = dropShadows.fold(isolateDropShadows(dropShadows)) { modifier, shadow ->
         modifier.dropShadow(
             shape = shape,
-            shadow = shadow.toComposeShadow(backgroundColor.alpha)
+            shadow = shadow.toComposeShadow(fillAlpha)
         )
     }
     val withoutDropShadowInside = if (dropShadows.isEmpty()) {
@@ -31,13 +44,17 @@ fun Modifier.elevation(
         withDropShadows.clearDropShadowInside(shape)
     }
 
-    val withBackground = withoutDropShadowInside.background(backgroundColor, shape)
+    val withBackground = withoutDropShadowInside.background(
+        brush = backgroundBrush,
+        shape = shape,
+        alpha = backgroundAlpha
+    )
 
     return elevation.shadows.fold(withBackground) { modifier, shadow ->
         if (shadow.type == ShadowType.Inner) {
             modifier.innerShadow(
                 shape = shape,
-                shadow = shadow.toComposeShadow(backgroundColor.alpha)
+                shadow = shadow.toComposeShadow(fillAlpha)
             )
         } else {
             modifier
