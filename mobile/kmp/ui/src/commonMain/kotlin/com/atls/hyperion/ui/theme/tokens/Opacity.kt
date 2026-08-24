bdccb3ecd10a5eb64000a33bedceb3bd6cd4af94
Arina Gazhina <gazhina@internet.ru>
2026-08-24 21:29:51 +0300

    feat(theme): opacity mobile

---
+++ mobile/kmp/ui/src/commonMain/kotlin/com/atls/hyperion/ui/theme/tokens/Opacity.kt
@@ -0,0 +1,14 @@
+package com.atls.hyperion.ui.theme.tokens
+
+object Opacity {
+    val none = 0f
+    val xs2 = 0.04f
+    val xs = 0.08f
+    val sm = 0.12f
+    val md = 0.16f
+    val lg = 0.24f
+    val xl = 0.4f
+    val xl2 = 0.64f
+    val xl3 = 0.8f
+    val full = 1f
+}
