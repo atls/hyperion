17a6d9ba2b24fb8937d5143560b933f2592876d0
Arina Gazhina <gazhina@internet.ru>
2026-08-24 21:10:24 +0300

    fix(theme): remove invalid elevation immutability

--- mobile/kmp/ui/src/commonMain/kotlin/com/atls/hyperion/ui/theme/tokens/elevation/Interfaces.kt
+++ mobile/kmp/ui/src/commonMain/kotlin/com/atls/hyperion/ui/theme/tokens/elevation/Interfaces.kt
@@ -4,7 +4,6 @@ import androidx.compose.runtime.Immutable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 
-@Immutable
 data class Elevation(
     val shadows: List<Shadow>
 )
