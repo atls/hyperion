3f61ab4799e1179dde418038ed2b9ae0472e13ae
Arina Gazhina <gazhina@internet.ru>
2026-08-27 20:46:46 +0300

    feat(button): add component stories

---
+++ ui-parts/button/stories/constants.ts
@@ -0,0 +1,16 @@
+export const appearances = ['primary', 'secondary', 'ghost'] as const
+
+export const appearanceLabels = {
+  primary: 'Primary',
+  secondary: 'Secondary',
+  ghost: 'Ghost',
+} as const
+
+export const shapes = ['xs', 'sm', 'md', 'lg'] as const
+
+export const shapeLabels = {
+  xs: 'Xs',
+  sm: 'Sm',
+  md: 'Md',
+  lg: 'Lg',
+} as const
---
+++ ui-parts/button/stories/styles.css.ts
@@ -0,0 +1,49 @@
+import { style }               from '@vanilla-extract/css'
+
+import { appearanceVariables } from '../src/styles/appearance/styles.css.js'
+import { shapeVariables }      from '../src/styles/shape/styles.css.js'
+
+export const containerStyles = style({
+  boxSizing: 'border-box',
+  display: 'grid',
+  gap: '32px',
+  width: '100%',
+  maxWidth: '720px',
+  padding: '32px',
+})
+
+export const appearanceStyles = style({
+  display: 'grid',
+  gap: '12px',
+})
+
+export const titleStyles = style({
+  margin: 0,
+  color: 'inherit',
+  fontFamily: 'inherit',
+  fontSize: '24px',
+  lineHeight: 1.2,
+})
+
+export const rowStyles = style({
+  display: 'grid',
+  gridTemplateColumns: '32px minmax(0, 1fr)',
+  alignItems: 'center',
+  gap: '16px',
+})
+
+export const shapeLabelStyles = style({
+  color: 'inherit',
+})
+
+export const buttonContainerStyles = style({
+  display: 'flex',
+  justifyContent: 'flex-end',
+})
+
+export const focusedStyles = style({
+  background: appearanceVariables.focused.background,
+  color: appearanceVariables.focused.content,
+  boxShadow: shapeVariables.elevations.focused,
+  outlineColor: appearanceVariables.focused.border,
+})
