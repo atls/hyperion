69e0bfd707b3e6e22209e193a23ed288958b218d
Arina Gazhina <gazhina@internet.ru>
2026-08-27 19:28:53 +0300

    fix(button): attenuate translucent elevations

--- ui-parts/button/src/styles/variables.ts
+++ ui-parts/button/src/styles/variables.ts
@@ -6,6 +6,7 @@ import type { ButtonShapeProperties }  from './shape/interfaces.js'
 import { assignInlineVars }            from '@vanilla-extract/dynamic'
 
 import { appearanceVariables }         from './appearance/styles.css.js'
+import { applyBackgroundAlpha }        from './elevation.js'
 import { shapeVariables }              from './shape/styles.css.js'
 
 export const assignButtonVariables = (
@@ -16,6 +17,13 @@ export const assignButtonVariables = (
   ...assignInlineVars(appearanceVariables, appearance),
   ...assignInlineVars(shapeVariables, {
     ...shape,
+    elevations: {
+      default: applyBackgroundAlpha(shape.elevations.default, appearance.default.background),
+      hover: applyBackgroundAlpha(shape.elevations.hover, appearance.hover.background),
+      pressed: applyBackgroundAlpha(shape.elevations.pressed, appearance.pressed.background),
+      disabled: applyBackgroundAlpha(shape.elevations.disabled, appearance.disabled.background),
+      focused: applyBackgroundAlpha(shape.elevations.focused, appearance.focused.background),
+    },
     typography: {
       fontFamily: String(shape.typography.fontFamily ?? 'inherit'),
       fontSize: String(shape.typography.fontSize ?? 'inherit'),
